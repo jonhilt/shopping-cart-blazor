794aa50ddab8e532f1bace50db3c0fc5849e9e30
Jon Hilton <hilton.jon@gmail.com>
2019-11-29 22:37:00 +0000

    Removed redundant namespace

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ShoppingCartWithBackend.Backend.Cart;
 using ShoppingCartWithBackend.Data;
 
 namespace ShoppingCartWithBackend
